91a56b8673878fcb72fa06cddb6da282b24cb012
valerychi <valery.chi@gmail.com>
2022-04-06 11:23:45 -0400

    removed installation of argocd as it will already be installed on the image.

--- argocd-install/install.sh
+++ argocd-install/install.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-# Install argocd in k8s cluster
-
-sudo microk8s kubectl create namespace argocd
-sudo microk8s kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# Apply cluster configs e.g secrets, configmaps and storageclass
 sudo microk8s kubectl apply -f /etc/argocd/data-gateway-config.yaml
